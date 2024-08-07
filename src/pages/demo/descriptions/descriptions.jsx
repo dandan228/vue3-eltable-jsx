98a6407ac5f08135c6ea4c321f5956a28e0681bc
const <constconst51@gmail.com>
2024-08-07 15:20:41 +0800

    fix: Descriptions子组件根据key值再次渲染

--- src/pages/demo/descriptions/descriptions.jsx
+++ src/pages/demo/descriptions/descriptions.jsx
@@ -1,4 +1,4 @@
-import { defineComponent } from "vue";
+import { defineComponent, reactive, onMounted } from "vue";
 import { Descriptions } from "../../../dist/index.js";
 import { desInfo } from "./config.js";
 
@@ -8,10 +8,23 @@ export default defineComponent({
       console.log("desRightClick", item);
     };
 
+    const state = reactive({
+      data: {},
+      key: 1
+    })
+
+    onMounted(() => {
+      setTimeout(() => {
+        state.data = {tip: 666}
+        state.key = 2
+      }, 1000);
+    })
+
     return () => (
       <Descriptions
-        desInfo={desInfo}
+        desInfo={desInfo(state.data)}
         column={4}
+        key={state.key}
         onDesRightClick={desRightClick}
       ></Descriptions>
     );
