584f63d47ce8f118fbd6a2e5a988fce1fb838000
const <constconst51@gmail.com>
2024-08-09 16:20:59 +0800

    style: upload示例

---
+++ src/pages/demo/Dialog/Dialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <Form :formColumns="formColumns"></Form>
+</template>
+
+<script setup>
+import { Form } from "../../../dist/index.js";
+import { Search } from "@element-plus/icons-vue";
+
+const formColumns = [  
+  {
+    prop: "date",
+    label: "input",
+    filterType: "upload",
+    actionUrl: "",
+    limit: 2,
+    fileList: [
+      { url: "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100" },
+    ],
+  },
+  {
+    filterType: "btn",
+    btnArr: [
+      { btnId: 0, label: "查询", color: "primary", icon: Search },
+      { btnId: 1, label: "禁用", color: "warning", disabled: true },
+    ],
+  },
+];
+</script>
